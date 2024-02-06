d546739253c0fd61f87df8725f8067f8def2fa31
“tekallus <dorukkuheylan@gmail.com>
2024-02-06 17:56:53 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        <span>Evil</span> Good Corp.
+      </h1>
+      <div>
+        <h3>
+          <span>Çalışan Gözetleme Programı</span>
+        </h3>
+        <h3>Masum Rapor Yazım Arayüzü</h3>
+      </div>
+    </header>
+  )
+}
